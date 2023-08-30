4a7d0af1fbbc6ff6cebefb45026cde16d9f3bbc1
amberhasan <amber.hasan@gmail.com>
2023-08-30 12:25:05 -0500

    Basic functionality of video works

--- src/components/TaegeukDetailsComponent.tsx
+++ src/components/TaegeukDetailsComponent.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import Video from 'react-native-video';
+import {Video} from 'expo-av';
 
 const TaegeukDetailsComponent = ({
   currentMove,
@@ -11,26 +11,24 @@ const TaegeukDetailsComponent = ({
   isPreviousDisabled,
   isNextDisabled,
 }) => {
+  const playerRef = useRef(null);
   return (
     <View style={styles.container}>
-      <Video
-        source={require('./test.mp4')}
-        style={{
-          width: 1000,
-          height: 1000,
-          flex: 1,
-        }}
-        resizeMode={'stretch'}
-      />
-
-      {/* <View style={styles.moveContainer}>
+      <View style={styles.moveContainer}>
         <Text style={styles.formText}>
           {currentMove.id}. {currentMove.moveDescription}
         </Text>
         <Text style={styles.descriptionText}>({currentMove.stance})</Text>
       </View>
       <View style={styles.imageContainer}>
-        <Video source={require('./test.mp4')} style={styles.video} />
+        <Video
+          ref={playerRef} // Store reference
+          source={{
+            uri: 'https://file-examples.com/storage/fead1d809b64e7bcd9ab4f1/2017/04/file_example_MP4_480_1_5MG.mp4',
+          }}
+          shouldPlay={true}
+          style={styles.video}
+        />
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity
@@ -48,7 +46,7 @@ const TaegeukDetailsComponent = ({
           disabled={isNextDisabled}>
           <MaterialIcons name="keyboard-arrow-right" size={24} color="white" />
         </TouchableOpacity>
-      </View> */}
+      </View>
     </View>
   );
 };
@@ -56,9 +54,9 @@ const TaegeukDetailsComponent = ({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // backgroundColor: '#f7f7f7',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f7f7f7',
   },
   moveContainer: {
     alignItems: 'center',
@@ -74,7 +72,6 @@ const styles = StyleSheet.create({
     color: '#666',
   },
   imageContainer: {
-    flex: 1,
     width: 300,
     height: 200,
     backgroundColor: 'red',
@@ -87,14 +84,8 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   video: {
-    // width: '100%',
-    // height: '100%',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    zIndex: 100,
+    width: '100%',
+    height: '100%',
   },
   buttonContainer: {
     flexDirection: 'row',
