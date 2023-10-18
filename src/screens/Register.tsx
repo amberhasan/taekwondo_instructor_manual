768711ff27f8015ef0dee28e0ed5ff37a3249975
Amber Hasan <amber.hasan@gmail.com>
2023-10-17 21:44:39 -0500

    Fixed Register page

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -25,7 +25,7 @@ const Register = ({navigation}) => {
         Alert.alert('Error', 'Email and password is required');
       }
     } catch (err) {
-      console.error('error', err);
+      Alert.alert('Error', err.message);
     }
   };
 
@@ -36,37 +36,34 @@ const Register = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Register</Text>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Display Name"
-          placeholderTextColor="#aaa"
-          onChangeText={text => setDisplayName(text)}
-          value={displayName}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          placeholderTextColor="#aaa"
-          onChangeText={text => setEmail(text)}
-          value={email}
-          keyboardType="email-address"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          placeholderTextColor="#aaa"
-          secureTextEntry={true}
-          onChangeText={text => setPassword(text)}
-          value={password}
-        />
-      </View>
-      <TouchableOpacity style={styles.button} onPress={handleRegister}>
+      <Text style={styles.subtitle}>Create a new account</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Display Name"
+        placeholderTextColor="#aaa"
+        onChangeText={text => setDisplayName(text)}
+        value={displayName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#aaa"
+        onChangeText={text => setEmail(text)}
+        value={email}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#aaa"
+        secureTextEntry={true}
+        onChangeText={text => setPassword(text)}
+        value={password}
+      />
+      <TouchableOpacity style={styles.buttonPrimary} onPress={handleRegister}>
         <Text style={styles.buttonText}>Register</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        style={[styles.button, styles.buttonSecondary]}
-        onPress={handleLogin}>
+      <TouchableOpacity style={styles.buttonSecondary} onPress={handleLogin}>
         <Text style={styles.buttonTextSecondary}>Login</Text>
       </TouchableOpacity>
     </View>
@@ -78,52 +75,60 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#6A11CB',
+    padding: 20,
+    backgroundColor: '#F5F5F5',
   },
   title: {
     fontSize: 28,
-    marginBottom: 40,
-    color: 'white',
     fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 10,
   },
-  inputContainer: {
-    width: '85%',
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
     marginBottom: 30,
   },
   input: {
     width: '100%',
-    height: 45,
-    borderColor: '#ffffff33',
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingLeft: 15,
-    borderRadius: 8,
-    backgroundColor: 'rgba(255, 255, 255, 0.1)',
-    color: 'white',
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    fontSize: 16,
+    color: '#333',
+    marginBottom: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
-  button: {
-    width: '80%',
-    height: 45,
-    backgroundColor: '#ffffff',
+  buttonPrimary: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#3498db',
+    borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 8,
     marginBottom: 15,
   },
   buttonSecondary: {
-    backgroundColor: 'transparent',
-    borderWidth: 1,
-    borderColor: '#ffffff',
+    alignItems: 'center',
+    marginBottom: 10,
   },
   buttonText: {
-    fontSize: 16,
-    fontWeight: '500',
-    color: '#6A11CB',
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
   },
   buttonTextSecondary: {
     fontSize: 16,
-    fontWeight: '500',
-    color: 'white',
+    color: '#3498db',
+    textDecorationLine: 'underline',
   },
 });
 
