d0d891ca9f8578639b6dfd3978ce76028ec1f609
Amber Hasan <amber.hasan@gmail.com>
2023-10-17 21:40:10 -0500

    Fixing profile page

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -33,6 +33,13 @@ const ProfileScreen = () => {
 
   const defaultProfilePic = require('../assets/images/profile_pic_default.png');
 
+  const dummyData = {
+    location: 'San Francisco, CA',
+    dateOfBirth: 'January 1, 1990',
+    joinedDate: 'May 15, 2020',
+    bio: 'Just a random user exploring the vast world of mobile apps.',
+  };
+
   const handleProfilePictureUpload = async () => {
     try {
       const result = await launchImageLibrary({
@@ -81,6 +88,24 @@ const ProfileScreen = () => {
         )}
       </TouchableOpacity>
       <Text style={styles.userEmail}>{user?.email}</Text>
+      <View style={styles.infoSection}>
+        <Text style={styles.infoLabel}>Location:</Text>
+        <Text style={styles.infoText}>San Francisco, CA</Text>
+      </View>
+      <View style={styles.infoSection}>
+        <Text style={styles.infoLabel}>Date of Birth:</Text>
+        <Text style={styles.infoText}>January 1, 1990</Text>
+      </View>
+      <View style={styles.infoSection}>
+        <Text style={styles.infoLabel}>Joined:</Text>
+        <Text style={styles.infoText}>May 15, 2020</Text>
+      </View>
+      <View style={styles.infoSection}>
+        <Text style={styles.infoLabel}>Bio:</Text>
+        <Text style={styles.infoText}>
+          Just a random user exploring the vast world of mobile apps.
+        </Text>
+      </View>
       <TouchableOpacity onPress={handleSignOut} style={styles.signOutButton}>
         <Text style={styles.signOutButtonText}>Sign Out</Text>
       </TouchableOpacity>
@@ -91,20 +116,26 @@ const ProfileScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
-    alignItems: 'center',
-    paddingTop: 50,
+    backgroundColor: '#F5F5F5', // Lighter background
+    paddingHorizontal: 20,
+    paddingTop: 20, // Reduced the top padding
   },
   header: {
-    marginBottom: 30,
+    marginBottom: 10, // Reduced margin below the header
   },
   headerText: {
-    fontSize: 26,
+    fontSize: 28,
     fontWeight: 'bold',
     color: '#333',
+    alignSelf: 'center',
   },
   avatarContainer: {
-    marginBottom: 20,
+    alignSelf: 'center',
+    marginBottom: 15, // Reduced margin below the avatar
+    borderWidth: 2,
+    borderColor: '#E1E1E1',
+    borderRadius: 70,
+    overflow: 'hidden',
   },
   avatarPlaceholder: {
     width: 130,
@@ -122,29 +153,60 @@ const styles = StyleSheet.create({
   profilePicture: {
     width: 130,
     height: 130,
-    borderRadius: 65,
-  },
-  userName: {
-    fontSize: 20,
-    fontWeight: '600',
-    color: '#333',
-    marginBottom: 5,
   },
   userEmail: {
     fontSize: 16,
     color: '#777',
-    marginBottom: 30,
+    marginBottom: 20,
+    alignSelf: 'center',
+  },
+  infoSection: {
+    flexDirection: 'row',
+    marginVertical: 8,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 12,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+  infoLabel: {
+    fontWeight: '600',
+    color: '#555',
+    flex: 1,
+  },
+  infoText: {
+    color: '#777',
+    fontSize: 16,
+    flex: 2,
+    textAlign: 'right',
   },
   signOutButton: {
-    backgroundColor: '#FF6347', // Tomato color for visual appeal
+    backgroundColor: '#FF6347',
     padding: 12,
     borderRadius: 25,
     paddingHorizontal: 30,
     marginTop: 20,
+    alignSelf: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
   },
   signOutButtonText: {
     color: '#FFF',
     fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
